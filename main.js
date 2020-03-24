e645e18d6f9eae755ab89c4d717aad50ee0ab5b9
mgmii <mgmii@umich.edu>
2020-03-24 16:55:00 -0400

    Added hover and debugged navigation bar color transition

---
+++ main.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", onPageLoadNavigation);
+
+function onPageLoadNavigation() {
+    const header = document.getElementById('header');
+    var hovered;
+
+    header.addEventListener("mouseover", function(){
+        header.classList.add('black');
+        console.log("hover");
+    });
+    
+    window.addEventListener('scroll', function(){
+        if(window.pageYOffset > 0){
+            if(header.className != "black") {
+                header.classList.add('black');
+                this.console.log("run");
+            }
+        }
+        else {
+            header.classList.remove('black');
+        }
+    });
+}
+
