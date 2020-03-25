9555546f4f33b4bb8e072853290bd14f8b0fd969
mgmii <mgmii@umich.edu>
2020-03-24 21:22:01 -0400

    Added capabilities for project drop down. Faulty java script for when drop down disappears

--- main.js
+++ main.js
@@ -18,4 +18,20 @@ function onPageLoadNavigation() {
             header.classList.remove('black');
         }
     });
+
+    const projects = document.getElementById('projects');
+    const projectContainer = document.getElementsByClassName("projects-menu-container");
+    const projectBack = document.getElementsByClassName('back-container');
+    
+    projects.addEventListener("mouseover", function(){
+        console.log(projectContainer[0].className);
+        if(projectContainer[0].className == "projects-menu-container") {
+            projectContainer[0].classList.add('active');
+            
+            projectContainer[0].addEventListener("mouseover",function(){
+                projectContainer[0].classList.remove('active');
+            });
+            console.log("Exited for");
+        }
+    });
 }
