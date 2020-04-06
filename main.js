bbbf69546b2871ae1adbff7fd18f1582b5693eb3
mmartin4972 <mmartin4972@gmail.com>
2020-04-06 10:14:23 -0400

    Added Event listener clean up for when transitioning between mobile and desktop

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 window.addEventListener("DOMContentLoaded", loadPage);
 window.addEventListener("resize", navigation);
 
+/* --- Helpful Variables ---*/
+const menu = document.getElementsByClassName('menu');
+const projects = document.getElementsByClassName('projects');
+const mrover = document.getElementsByClassName('mrover');
+const logoDesktop = document.getElementsByClassName('logo-desktop');
+const logoMobile = document.getElementsByClassName('logo-mobile');
+const mobileMenuToggle = document.getElementsByClassName('mobile-menu-toggle');
+var viewportwidth;
+const projectsId = document.getElementById('projects');
+const projectContainer = document.getElementsByClassName("projects menu-container");
+const projectBack = document.getElementsByClassName('projects back-container');
+const logo = document.getElementsByClassName('logo-desktop');
+const mroverId = document.getElementById('mrover');
+const mroverDrop = document.getElementsByClassName('mrover');
+const scouts = document.getElementById('scouts');
+const first = document.getElementById('first');
+const mobileDropDown = document.getElementsByClassName('mobile-dropdown');
+const menuToggle = document.getElementsByClassName('menu-toggle');
+var mobileInitialized = false;
+var desktopInitialized = false;
+var headerInitialized = false;
+
 function loadPage() {    
     window.setTimeout(killLoadPage, 1000);
 }
@@ -16,25 +38,68 @@ function killLoadPage() {
     navigation();
 }
 
+/*--- Mobile Functions ---*/
+var addWhite = function(event) {
+    if(!menuToggle[0].classList.contains('menu-toggle-white')){
+        menuToggle[0].classList.add('menu-toggle-white');}
+}
+var removeWhite = function(event){
+    if(menuToggle[0].classList.contains('menu-toggle-white')) {
+        menuToggle[0].classList.remove('menu-toggle-white');}
+}
+var dropShow = function(event){
+    if(!mobileDropDown[0].classList.contains('active')){
+        mobileDropDown[0].classList.add('active');
+        console.log('clicked')}
+    else{
+        mobileDropDown[0].classList.remove('active');
+        console.log('clicked');
+    }   
+} 
+
+/*--- Desktop Functions ---*/
+
+/*--- Turns off Project Drop Down ---*/
+function turnOffProjectDropDown(){
+    projectContainer[0].classList.remove('active');
+    projectBack[0].removeEventListener('mouseleave',turnOffProjectDropDown);
+    scouts.removeEventListener("mouseover", turnOffProjectDropDown);
+    first.removeEventListener("mouseover", turnOffProjectDropDown);
+    document.removeEventListener("mouseleave", turnOffProjectDropDown);
+}
+
+/*--- Turns off MROVER Drop Down ---*/
+function turnOffMROVERDropDown(){
+    mroverDrop[0].classList.remove('active');
+    mroverDrop[1].removeEventListener('mouseleave',turnOffMROVERDropDown);
+    first.removeEventListener("mouseover", turnOffMROVERDropDown);
+    logo[0].removeEventListener("mouseover", turnOffMROVERDropDown);
+    document.removeEventListener("mouseleave", turnOffMROVERDropDown);
+}
+
+/*--- Projects Drop Down Function ---*/
+function projectDropDown() {
+    if(!projectContainer[0].classList.contains("active")) {
+        projectContainer[0].classList.add('active');
+        projectBack[0].addEventListener("mouseleave",turnOffProjectDropDown);
+        first.addEventListener("mouseover", turnOffProjectDropDown);
+        scouts.addEventListener("mouseover", turnOffProjectDropDown);
+        document.addEventListener("mouseleave", turnOffProjectDropDown);
+    }
+}
+
+/*--- MROVER Drop Down Function ---*/
+function mroverDropDown() {
+    if(!mroverDrop[0].classList.contains("active")) {
+        mroverDrop[0].classList.add('active');
+        mroverDrop[1].addEventListener("mouseleave",turnOffMROVERDropDown);
+        logo[0].addEventListener("mouseover", turnOffMROVERDropDown);
+        first.addEventListener("mouseover", turnOffMROVERDropDown);
+        document.addEventListener("mouseleave", turnOffMROVERDropDown);
+    }
+}
+
 function navigation() {
-    /* --- Helpful Variables ---*/
-    const menu = document.getElementsByClassName('menu');
-    const projects = document.getElementsByClassName('projects');
-    const mrover = document.getElementsByClassName('mrover');
-    const logoDesktop = document.getElementsByClassName('logo-desktop');
-    const logoMobile = document.getElementsByClassName('logo-mobile');
-    const mobileMenuToggle = document.getElementsByClassName('mobile-menu-toggle');
-    var viewportwidth;
-    const projectsId = document.getElementById('projects');
-    const projectContainer = document.getElementsByClassName("projects menu-container");
-    const projectBack = document.getElementsByClassName('projects back-container');
-    const logo = document.getElementsByClassName('logo-desktop');
-    const mroverId = document.getElementById('mrover');
-    const mroverDrop = document.getElementsByClassName('mrover');
-    const scouts = document.getElementById('scouts');
-    const first = document.getElementById('first');
-    const mobileDropDown = document.getElementsByClassName('mobile-dropdown');
-    const menuToggle = document.getElementsByClassName('menu-toggle');
     
     // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight   
      if (typeof window.innerWidth != 'undefined')
@@ -43,6 +108,7 @@ function navigation() {
      }
 
     //Create Header Styling
+    if(!headerInitialized) {
     const header = document.getElementById('header');
        
         header.addEventListener("mouseover", function(){
@@ -59,11 +125,21 @@ function navigation() {
                 header.classList.remove('black');
             }
         });
+        headerInitialized = true;
+        console.log('header initialized');
+    }
 
     //Mobile JavaScript
-    if(viewportwidth < 645) {
+    if(viewportwidth < 645 && !mobileInitialized) {
         
-
+        /*--- Clean Desktop Event Listeners ---*/
+        if(desktopInitialized) {
+            projectsId.addEventListener("mouseover", projectDropDown);
+            mroverId.addEventListener("mouseover", mroverDropDown);
+            desktopInitialized = false;
+            console.log('desktop cleaned');
+        }
+        
         /* --- Hide Desktop Menu Items ---*/
         if(!menu[0].classList.contains('hide')) {menu[0].classList.add('hide');}
         if(!projects[0].classList.contains('hide')) {projects[0].classList.add('hide');}
@@ -74,30 +150,28 @@ function navigation() {
         if(logoMobile[0].classList.contains('hide')) {logoMobile[0].classList.remove('hide');}
         if(mobileMenuToggle[0].classList.contains('hide')) {mobileMenuToggle[0].classList.remove('hide');}
 
-        mobileMenuToggle[0].addEventListener('mouseover', function() {
-            if(!menuToggle[0].classList.contains('menu-toggle-white')){
-                menuToggle[0].classList.add('menu-toggle-white');}
-        })
-
-        mobileMenuToggle[0].addEventListener('mouseleave', function() {
-            if(menuToggle[0].classList.contains('menu-toggle-white')) {
-                menuToggle[0].classList.remove('menu-toggle-white');}
-        })
-        mobileMenuToggle[0].addEventListener('click', function(){
-            if(!mobileDropDown[0].classList.contains('active')){
-                mobileDropDown[0].classList.add('active');}
-            else{
-                mobileDropDown[0].classList.remove('active');
-            }
-            
-        })
-       
-
+      
+        
+        /*--- Event Listeners ---*/
+        mobileMenuToggle[0].addEventListener('mouseover', addWhite, true);
+        mobileMenuToggle[0].addEventListener('mouseleave', removeWhite, true);
+        mobileMenuToggle[0].addEventListener('click', dropShow, true);
+        mobileInitialized = true;
+        console.log('mobile initialized');
     }
 
     //Desktop JavaScript
-    else {
-
+    if(viewportwidth >= 645 && !desktopInitialized) {
+       
+        /*--- Kill Mobile Event Listeners ---*/
+        if(mobileInitialized){
+            mobileInitialized = false;
+            mobileMenuToggle[0].removeEventListener('mouseover', addWhite, true);
+            mobileMenuToggle[0].removeEventListener('mouseleave', removeWhite, true);
+            mobileMenuToggle[0].removeEventListener('click', dropShow, true);
+            console.log('mobile cleaned');
+        }
+        
         /*--- Show Desktop Menu Items ---*/ 
         if(menu[0].classList.contains('hide')) {menu[0].classList.remove('hide');}
         if(projects[0].classList.contains('hide')) {projects[0].classList.remove('hide');}
@@ -108,44 +182,12 @@ function navigation() {
         if(!logoMobile[0].classList.contains('hide')) {logoMobile[0].classList.add('hide');}
         if(!mobileMenuToggle[0].classList.contains('hide')) {mobileMenuToggle[0].classList.add('hide');}
 
-        /*--- Turns off Project Drop Down ---*/
-        function turnOffProjectDropDown(){
-            projectContainer[0].classList.remove('active');
-            projectBack[0].removeEventListener('mouseleave',turnOffProjectDropDown);
-            scouts.removeEventListener("mouseover", turnOffProjectDropDown);
-            first.removeEventListener("mouseover", turnOffProjectDropDown);
-            document.removeEventListener("mouseleave", turnOffProjectDropDown);
-        }
-
-        /*--- Turns off MROVER Drop Down ---*/
-        function turnOffMROVERDropDown(){
-            mroverDrop[0].classList.remove('active');
-            mroverDrop[1].removeEventListener('mouseleave',turnOffMROVERDropDown);
-            first.removeEventListener("mouseover", turnOffMROVERDropDown);
-            logo[0].removeEventListener("mouseover", turnOffMROVERDropDown);
-            document.removeEventListener("mouseleave", turnOffMROVERDropDown);
-        }
-
-        /*--- Projects Drop Down Function ---*/
-        projectsId.addEventListener("mouseover", function(){
-            if(!projectContainer[0].classList.contains("active")) {
-                projectContainer[0].classList.add('active');
-                projectBack[0].addEventListener("mouseleave",turnOffProjectDropDown);
-                first.addEventListener("mouseover", turnOffProjectDropDown);
-                scouts.addEventListener("mouseover", turnOffProjectDropDown);
-                document.addEventListener("mouseleave", turnOffProjectDropDown);
-            }
-        });
-
-        /*--- MROVER Drop Down Function ---*/
-        mroverId.addEventListener("mouseover", function(){
-            if(!mroverDrop[0].classList.contains("active")) {
-                mroverDrop[0].classList.add('active');
-                mroverDrop[1].addEventListener("mouseleave",turnOffMROVERDropDown);
-                logo[0].addEventListener("mouseover", turnOffMROVERDropDown);
-                first.addEventListener("mouseover", turnOffMROVERDropDown);
-                document.addEventListener("mouseleave", turnOffMROVERDropDown);
-            }
-        });
+        
+        /*--- Add Event Listeners ---*/
+        projectsId.addEventListener("mouseover", projectDropDown);
+        mroverId.addEventListener("mouseover", mroverDropDown);
+        desktopInitialized = true;
+        console.log('desktop initialized');
     }
+    return;
 }
